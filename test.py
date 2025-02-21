ea4319212fa7a4f14c091da295af06c9aafa0888
Timur Carstensen <timurcarstensen@gmail.com>
2025-02-21 10:37:38 +0100

    add small test script

--- test.py
+++ test.py
@@ -2,15 +2,16 @@ import gc
 import os
 import time
 from dataclasses import dataclass
-from typing import Literal
 
 import numpy as np
 import torch
 import torch.nn as nn
-import torchvision.models as models
+import torchvision
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
 from tqdm import tqdm
+
+from layer_freeze.model_agnostic_freezing import FrozenModel
 
 
 @dataclass
@@ -22,94 +23,7 @@ class ModelPerfStats:
     median_loop_time: float
     memory: float
     frac_trainable_params: float
-
-
-class FrozenModel(nn.Module):
-    def __init__(
-        self,
-        n_classes: int,
-        n_trainable: int = 1,
-        quantize_frozen_layers: bool = False,
-        base_model: Literal[
-            "resnet18", "resnet34", "resnet50", "resnet101", "resnet152"
-        ] = "resnet18",
-    ) -> None:
-        super().__init__()
-
-        match base_model:
-            case "resnet18":
-                self.model = models.resnet18(weights=None)
-            case "resnet34":
-                self.model = models.resnet34(weights=None)
-            case "resnet50":
-                self.model = models.resnet50(weights=None)
-            case "resnet101":
-                self.model = models.resnet101(weights=None)
-            case "resnet152":
-                self.model = models.resnet152(weights=None)
-            case _:
-                raise ValueError(f"Invalid base model: {base_model}")
-
-        all_layers: list[tuple] = []
-        for child in self.model.children():
-            match child:
-                case nn.Sequential():
-                    for subchild in child.children():
-                        all_layers.append((subchild, None))
-                case _:
-                    all_layers.append((child, None))
-
-        for i, (layer, _) in enumerate(all_layers):
-            all_layers[i] = (layer, bool(list(layer.parameters())))
-
-        self.n_layers_with_params = sum(1 for _, has_params in all_layers if has_params)
-        if n_trainable > self.n_layers_with_params:
-            raise ValueError(
-                f"n_trainable is greater than the number of trainable layers: "
-                f"{self.n_layers_with_params}"
-            )
-
-        ctr = 0
-        trainable_layers = []
-        for _, (layer, has_params) in enumerate(all_layers[::-1]):
-            if has_params:
-                ctr += 1
-
-            if ctr > n_trainable:
-                break
-
-            trainable_layers.append(layer)
-
-        frozen_layers = [layer for layer, _ in all_layers if layer not in trainable_layers]
-
-        if len(trainable_layers) > 0:
-            trainable_layers.insert(1, nn.Flatten(1))
-
-            if n_classes != 1000:
-                trainable_layers.remove(trainable_layers[0])
-                trainable_layers.insert(0, nn.LazyLinear(n_classes))
-
-        self.frozen_layers = nn.Sequential(*frozen_layers)
-        self.trainable_layers = nn.Sequential(*trainable_layers[::-1])
-
-        for param in self.frozen_layers.parameters():
-            param.requires_grad = False
-
-        for param in self.trainable_layers.parameters():
-            param.requires_grad = True
-
-        self.quantize_frozen_layers = quantize_frozen_layers
-        if quantize_frozen_layers:
-            self.frozen_layers = self.frozen_layers.to(torch.float16)
-
-    def forward(self, x):
-        if self.quantize_frozen_layers:
-            with torch.autocast(device_type="cuda", dtype=torch.float16):
-                x = self.frozen_layers(x)
-        else:
-            x = self.frozen_layers(x)
-        with torch.autocast(device_type="cuda", dtype=torch.float32):
-            return self.trainable_layers(x)
+    optimizer_time: float
 
 
 def frac_trainable_params(model: nn.Module) -> float:
@@ -119,7 +33,7 @@ def frac_trainable_params(model: nn.Module) -> float:
 
 
 def model_perf_stats(
-    model,
+    model: nn.Module,
     dataloader: "DataLoader",
     optimizer: torch.optim.Optimizer,
     warmup_passes: int = 100,
@@ -146,24 +60,30 @@ def model_perf_stats(
     fwd_times = []
     bwd_times = []
     loop_times = []
+    optimizer_times = []
     max_memory_usage_recorded = 0
     for batch in tqdm(dataloader, desc="Measuring forward times"):
-        loop_start_time = time.perf_counter()
+        loop_start = time.perf_counter()
         x, y = batch
         x = x.to(device)
         y = y.to(device)
-        start_time = time.perf_counter()
-        pred = model(x)
-        fwd_times.append(time.perf_counter() - start_time)
 
+        fwd_time = time.perf_counter()
+        pred = model(x)
         loss = torch.nn.functional.cross_entropy(pred, y)
-        start_time = time.perf_counter()
         loss = loss.to(device)
+        fwd_times.append(time.perf_counter() - fwd_time)
+
+        bwd_time = time.perf_counter()
         loss.backward()
-        bwd_times.append(time.perf_counter() - start_time)
+        bwd_times.append(time.perf_counter() - bwd_time)
+
+        opt_start = time.perf_counter()
         optimizer.step()
         optimizer.zero_grad()
-        loop_times.append(time.perf_counter() - loop_start_time)
+        optimizer_times.append(time.perf_counter() - opt_start)
+
+        loop_times.append(time.perf_counter() - loop_start)
         max_memory_usage_recorded = max(
             max_memory_usage_recorded, torch.cuda.memory_allocated(device) / (1024**2)
         )
@@ -177,6 +97,7 @@ def model_perf_stats(
         median_loop_time=np.median(loop_times),
         memory=max_memory_usage_recorded,
         frac_trainable_params=frac_trainable_params(model),
+        optimizer_time=np.median(optimizer_times),
     )
 
 
@@ -204,15 +125,13 @@ if __name__ == "__main__":
         persistent_workers=True,
     )
 
-    stats = []
-    for n_trainable in range(
-        1, FrozenModel(n_classes=10, base_model=args.base_model).n_layers_with_params
-    ):
+    stats: list[ModelPerfStats] = []
+    for n_trainable in range(1, 10):
         model = FrozenModel(
-            n_classes=10,
             n_trainable=n_trainable,
-            quantize_frozen_layers=args.quantize_frozen_layers,
-            base_model=args.base_model,
+            base_model=torchvision.models.vit_b_16(weights=None),
+            print_summary=True,
+            unwrap=torchvision.models.vision_transformer.Encoder,
         )
         optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
         stats.append(
@@ -223,7 +142,7 @@ if __name__ == "__main__":
         torch.cuda.empty_cache()
         gc.collect()
 
-    fig, ax1 = plt.subplots(figsize=(7, 7))
+    fig, ax1 = plt.subplots(figsize=(10, 6))
     ax2 = ax1.twinx()  # Create second y-axis sharing same x-axis
 
     # Plot forward and backward times on left y-axis
@@ -249,6 +168,14 @@ if __name__ == "__main__":
         linestyle="--",
     )
 
+    ax1.plot(
+        [stat.frac_trainable_params for stat in stats],
+        [stat.optimizer_time for stat in stats],
+        label="Optimizer step + zero grad",
+        marker="o",
+        linestyle="--",
+    )
+
     ax1.set_xlabel("Fraction of Trainable Parameters")
     ax1.set_ylabel("Time (s)")
 
@@ -267,7 +194,7 @@ if __name__ == "__main__":
     lines2, labels2 = ax2.get_legend_handles_labels()
     ax1.legend(lines1 + lines2, labels1 + labels2, loc="upper left")
 
-    base_path = "./results/model_perf/quantized_frozen_layers"
+    base_path = "./results/model_perf/vit/quantized_frozen_layers"
     os.makedirs(base_path, exist_ok=True)
     plt.grid(True, axis="both", linestyle="--", alpha=0.4, which="major")
     plt.title(
