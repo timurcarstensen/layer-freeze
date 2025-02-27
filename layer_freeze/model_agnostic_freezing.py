b975e7e439cbb434a4fa561ccf4f549b6cf11496
Timur Carstensen <timurcarstensen@gmail.com>
2025-02-27 16:45:58 +0100

    misc

--- layer_freeze/model_agnostic_freezing.py
+++ layer_freeze/model_agnostic_freezing.py
@@ -2,39 +2,9 @@ from typing import Any
 
 import rich
 import torch.nn as nn
-from torchvision import models
 
 
 class FrozenModel(nn.Module):
-    @staticmethod
-    def _split_layers_and_freeze(
-        layers: list[tuple[nn.Module, bool]], n_trainable: int
-    ) -> tuple[list[nn.Module], list[nn.Module]]:
-        ctr = 0
-        trainable: list[nn.Module] = []
-        for _, (layer, has_params) in enumerate(layers[::-1]):
-            if has_params:
-                ctr += 1
-
-            if ctr > n_trainable:
-                break
-
-            trainable.insert(0, layer)
-
-        frozen: list[nn.Module] = [
-            layer for layer, _ in layers if layer not in trainable
-        ]
-
-        for layer in frozen:
-            for param in layer.parameters():
-                param.requires_grad = False
-
-        for layer in trainable:
-            for param in layer.parameters():
-                param.requires_grad = True
-
-        return frozen, trainable
-
     def __init__(
         self,
         base_model: nn.Module,
@@ -63,7 +33,7 @@ class FrozenModel(nn.Module):
 
         all_layers: list[tuple] = []
 
-        def _recursive_children(module: nn.Module, unwrap: Any = None) -> None:
+        def _recursive_children(module: nn.Module, unwrap: Any | list[Any] = None) -> None:
             for child in module.children():
                 match child:
                     case nn.Sequential():
@@ -93,14 +63,39 @@ class FrozenModel(nn.Module):
                 f"{n_layers_with_params}"
             )
 
-        self.frozen, self.trainable = self._split_layers_and_freeze(
-            self.all_layers, n_trainable
-        )
+        self.frozen, self.trainable = self._split_layers_and_freeze(self.all_layers, n_trainable)
 
         assert len(self.frozen) + len(self.trainable) == len(self.all_layers)
 
         if print_summary:
             self.print_layers(frozen=self.frozen, trainable=self.trainable)
+
+    @staticmethod
+    def _split_layers_and_freeze(
+        layers: list[tuple[nn.Module, bool]], n_trainable: int
+    ) -> tuple[list[nn.Module], list[nn.Module]]:
+        ctr = 0
+        trainable: list[nn.Module] = []
+        for _, (layer, has_params) in enumerate(layers[::-1]):
+            if has_params:
+                ctr += 1
+
+            if ctr > n_trainable:
+                break
+
+            trainable.insert(0, layer)
+
+        frozen: list[nn.Module] = [layer for layer, _ in layers if layer not in trainable]
+
+        for layer in frozen:
+            for param in layer.parameters():
+                param.requires_grad = False
+
+        for layer in trainable:
+            for param in layer.parameters():
+                param.requires_grad = True
+
+        return frozen, trainable
 
     def thaw(self, n: int = 1):
         """
@@ -147,15 +142,11 @@ class FrozenModel(nn.Module):
 
         rich.print("\n[bold blue]Frozen Layers:[/bold blue]")
         for layer in _frozen:
-            rich.print(
-                f"  {layer['name']}, trainable = False, params = {layer['num_params']:,}"
-            )
+            rich.print(f"  {layer['name']}, trainable = False, params = {layer['num_params']:,}")
 
         rich.print("\n[bold green]Trainable Layers:[/bold green]")
         for layer in _trainable:
-            rich.print(
-                f"  {layer['name']}, trainable = True, params = {layer['num_params']:,}"
-            )
+            rich.print(f"  {layer['name']}, trainable = True, params = {layer['num_params']:,}")
 
         total_frozen = sum(layer["num_params"] for layer in _frozen)
         total_trainable = sum(layer["num_params"] for layer in _trainable)
@@ -163,12 +154,3 @@ class FrozenModel(nn.Module):
         rich.print(f"  Total frozen parameters: {total_frozen:,}")
         rich.print(f"  Total trainable parameters: {total_trainable:,}")
         rich.print(f"  Total parameters: {total_frozen + total_trainable:,}")
-
-
-if __name__ == "__main__":
-    from torchvision.models.vision_transformer import Encoder
-
-    model = models.vit_b_16(weights=None)
-    frozen_model = FrozenModel(
-        base_model=model, n_trainable=3, unwrap=Encoder, print_summary=True
-    )
